43add7bf2554be3d34d55de4fb9c5f39cdc74ac2
Stephen Lazaro <stephen.lazaro@gmail.com>
2019-09-08 12:48:08 -0700

    Derive Invariant (#182)
    * Derive Invariant
    
    * Fix merge
    
    * Drop a couple tests
    
    * Fix an implicit bound in test
    
    * Incorporate feedback, add tests
    
    * Avoid forcing with `.value`
    
    * Hmm tests pass locally... deleting some trivial tests in invariant to
    kick off again

--- core/src/main/scala/cats/derived/package.scala
+++ core/src/main/scala/cats/derived/package.scala
@@ -54,7 +54,13 @@ object auto {
     ): Hash[A] = ev.value
   }
 
-  object contravariant {
+  object invariant {
+    implicit def kittensMkInvariant[F[_]](
+      implicit refute: Refute[Invariant[F]], F: Lazy[MkInvariant[F]]
+    ): Invariant[F] = F.value
+  }
+
+   object contravariant {
     implicit def kittensMkContravariant[F[_]](
       implicit refute: Refute[Contravariant[F]], F: Lazy[MkContravariant[F]]
     ): Contravariant[F] = F.value
@@ -217,6 +223,12 @@ object cached {
     ): Foldable[F] = cached.value
   }
 
+  object invariant {
+    implicit def kittensMkInvariant[F[_]](
+      implicit refute: Refute[Invariant[F]], cached: Cached[MkInvariant[F]]
+    ): Invariant[F] = cached.value
+  }
+
   object traverse{
     implicit def kittensMkTraverse[F[_]](
        implicit refute: Refute[Traverse[F]], cached: Cached[MkTraverse[F]]
@@ -340,5 +352,7 @@ object semi {
   def consK[F[_]](implicit F: Lazy[MkConsK[F, F]]): ConsK[F] = MkConsK.consK(F.value)
 
   def iterable[F[_], A](fa: F[A])(implicit F: MkIterable[F]): Iterable[A] = F.iterable(fa)
+
+  def invariant[F[_]](implicit F: Lazy[MkInvariant[F]]): Invariant[F] = F.value
 }
 
---
+++ core/src/test/scala/cats/derived/invariant.scala
@@ -0,0 +1,95 @@
+/*
+ * Copyright (c) 2015 Miles Sabin
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package cats
+package derived
+
+import cats.implicits._
+import cats.laws.discipline._
+import cats.laws.discipline.arbitrary._
+import cats.laws.discipline.eq._
+import cats.laws.discipline._
+
+class InvariantSuite extends KittensSuite {
+  import TestDefns._
+  import TestEqInstances._
+
+  type ListSnoc[A] = List[Snoc[A]]
+  type GenericAdtF[A] = GenericAdt[A => Boolean]
+  type ListFToInt[A] = List[Snoc[A => Int]]
+  type InterleavedF[A] = Interleaved[A => Boolean]
+  type AndCharF[A] = (A => Boolean, Char)
+  type TreeF[A] = Tree[A => Boolean]
+
+  def testInvariant(context: String)(
+    implicit
+    tree: Invariant[TreeF],
+    genadt: Invariant[GenericAdtF],
+    ListFToInt: Invariant[ListFToInt],
+    listSnoc: Invariant[ListSnoc],
+    interleaved: Invariant[InterleavedF],
+    andCharF: Invariant[AndCharF],
+    bivariant: Invariant[Bivariant],
+    ilist: Invariant[IList]
+  ): Unit = {
+    checkAll(s"$context.Invariant[TreeF]", InvariantTests[TreeF].invariant[MiniInt, String, Boolean])
+    checkAll(s"$context.Invariant[GenAdtF]", InvariantTests[GenericAdtF].invariant[MiniInt, String, Boolean])
+    checkAll(s"$context.Invariant[InterleavedF]", InvariantTests[InterleavedF].invariant[MiniInt, String, Boolean])
+    checkAll(s"$context.Invariant[AndCharF]", InvariantTests[AndCharF].invariant[MiniInt, String, Boolean])
+    checkAll(s"$context.Invariant[ListSnoc", InvariantTests[ListSnoc].invariant[MiniInt, String, Boolean])
+    checkAll(s"$context.Invariant[Bivariant]", InvariantTests[Bivariant].invariant[MiniInt, String, Boolean])
+
+    checkAll(s"$context.Invariant is Serializable", SerializableTests.serializable(Invariant[TreeF]))
+
+    test(s"$context.Invariant.imap is stack safe") {
+      val n = 10000
+      val largeIList = IList.fromSeq(1 until n)
+      val largeSnoc = Snoc.fromSeq(1 until n) :: Nil
+      val actualIList = IList.toList(largeIList.imap(_ + 1)(_ - 1))
+      val actualSnoc = listSnoc.imap(largeSnoc)(_ + 1)(_ - 1).flatMap(Snoc.toList)
+      val expected = (2 until n + 1).toList
+      assert(actualIList == expected)
+      assert(actualSnoc == expected)
+    }
+  }
+
+  {
+    import auto.invariant._
+    testInvariant("auto")
+  }
+
+  {
+    import cached.invariant._
+    testInvariant("cached")
+  }
+
+  semiTests.run()
+
+  object semiTests {
+    implicit val gadt: Invariant[GenericAdtF] = semi.invariant[GenericAdtF]
+    implicit val listSnocendo: Invariant[ListFToInt] = semi.invariant[ListFToInt]
+    implicit val interleaveF: Invariant[InterleavedF] = semi.invariant[InterleavedF]
+    implicit val andCharF: Invariant[AndCharF] = semi.invariant[AndCharF]
+    implicit val treeF: Invariant[TreeF] = semi.invariant[TreeF]
+    implicit val pred: Invariant[Pred] = semi.invariant[Pred]
+    implicit val snoc: Invariant[ListSnoc] = semi.invariant[ListSnoc]
+    implicit val bivariant: Invariant[Bivariant] = semi.invariant[Bivariant]
+    implicit val ilist: Invariant[IList] = semi.invariant[IList]
+
+    def run(): Unit = testInvariant("semi")
+  }
+}
+

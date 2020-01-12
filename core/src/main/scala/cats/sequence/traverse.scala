3a2712ea131f2145f8f90eefe000b3dcd9d37922
Georgi Krastev <joro.kr.21@gmail.com>
2020-01-12 22:25:02 +0100

    Add parTraverse similar to parSequence

--- core/src/main/scala/cats/sequence/traverse.scala
+++ core/src/main/scala/cats/sequence/traverse.scala
@@ -5,33 +5,53 @@
  */
 package cats.sequence
 
+import cats.Parallel
 import shapeless._
 import shapeless.ops.hlist._
 
 sealed trait Traverser[L <: HList, P] extends Serializable {
-  type Out
+  type F[_]
+  type LOut <: HList
+  type Out = F[LOut]
   def apply(hl: L): Out
+  protected[sequence] def par(hl: L)(implicit P: Parallel[F]): P.F[LOut]
+  final def parApply(hl: L)(implicit P: Parallel[F]): Out = P.sequential(par(hl)(P))
 }
 
 object Traverser {
-  type Aux[L <: HList, P, Out0] = Traverser[L, P] { type Out = Out0 }
+  type Aux[L <: HList, P, F0[_], LOut0 <: HList] = Traverser[L, P] {
+    type F[X] = F0[X]
+    type LOut = LOut0
+  }
 
   implicit def mkTraverser[L <: HList, P, S <: HList](
     implicit mapper: Mapper.Aux[P, L, S], sequencer: Sequencer[S]
-  ): Aux[L, P, sequencer.Out] = new Traverser[L, P] {
-    type Out = sequencer.Out
-    def apply(hl: L): Out = sequencer(mapper(hl))
+  ): Aux[L, P, sequencer.F, sequencer.LOut] = new Traverser[L, P] {
+    type F[X] = sequencer.F[X]
+    type LOut = sequencer.LOut
+    def apply(hl: L) = sequencer(mapper(hl))
+    def par(hl: L)(implicit P: Parallel[F]) = sequencer.par(mapper(hl))(P)
   }
 }
 
 trait TraverseFunctions {
   def traverse[L <: HList](hl: L)(f: Poly)(implicit traverser: Traverser[L, f.type]): traverser.Out =
     traverser(hl)
+
+  def parTraverse[F[_], L <: HList, LOut <: HList](hl: L)(f: Poly)(
+    implicit traverser: Traverser.Aux[L, f.type, F, LOut],
+    par: Parallel[F]
+  ): F[LOut] = traverser.parApply(hl)
 }
 
 trait TraverseOps extends {
   implicit class withTraverse[L <: HList](self: L) {
     def traverse(f: Poly)(implicit traverser: Traverser[L, f.type]): traverser.Out =
       traverser(self)
+
+    def parTraverse[F[_], LOut <: HList](f: Poly)(
+      implicit traverser: Traverser.Aux[L, f.type, F, LOut],
+      par: Parallel[F]
+    ): F[LOut] = traverser.parApply(self)
   }
 }

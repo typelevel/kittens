0cc9642817d2e0e2a67cfec8f5cfcea6f9077525
Tim Spence <timothywspence@gmail.com>
2021-05-19 14:12:45 +0100

    Use SAM functions to avoid anonymous class generation

--- core/src/main/scala-3.0.0-RC3/cats/derived/show.scala
+++ core/src/main/scala-3.0.0-RC3/cats/derived/show.scala
@@ -10,25 +10,23 @@ trait ShowDerivation:
   extension (F: Show.type)
     inline def derived[A](using gen: K0.Generic[A]): Show[A] = gen.derive(productShow, coproductShow)
 
-  given productShow[A](using inst : => K0.ProductInstances[Show, A], l: Labelling[A]): Show[A] with
-    def show(a: A) = {
+  given productShow[A](using inst : => K0.ProductInstances[Show, A], l: Labelling[A]): Show[A] =
+    (a: A) => {
       var idx = 0
-      var removeTrailingComma = false
       val x = inst.foldLeft[StringBuilder](a)(new StringBuilder(s"${l.label}("))(
         [t] => (acc: StringBuilder, sh: Show[t], t0: t) => {
           //elemLabels is backed by an array so this should be fast
           val res = Continue(acc.append(s"${l.elemLabels(idx)}=${sh.show(t0)}, "))
-          removeTrailingComma = true
           idx = idx + 1
           res
           }
       )
-      if(removeTrailingComma) {x.delete(x.length - 2, x.length)}
+      if(idx > 0) {x.delete(x.length - 2, x.length)}
       x.append(")")
       x.toString()
     }
 
-  given coproductShow[A](using inst: => K0.CoproductInstances[Show, A]): Show[A] with
-    def show(a: A) = inst.fold[String](a)(
+  given coproductShow[A](using inst: => K0.CoproductInstances[Show, A]): Show[A] =
+    (a: A) => inst.fold[String](a)(
       [t] => (sh: Show[t], t0: t) => sh.show(t0)
     )

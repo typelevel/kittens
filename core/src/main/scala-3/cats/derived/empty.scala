8a49e4ef629dd608a8385c6a2119d44545d6c986
Georgi Krastev <joro.kr.21@gmail.com>
2021-05-25 08:54:36 +0300

    Deep and Coproduct Empty derivation
    It looks like we will need `OrElse` again.

--- core/src/main/scala-3/cats/derived/empty.scala
+++ core/src/main/scala-3/cats/derived/empty.scala
@@ -5,12 +5,24 @@ import shapeless3.deriving.K0
 
 object empty extends EmptyDerivation
 
-class ProductEmpty[F[x] <: Empty[x], A](
-  using inst: K0.ProductInstances[F, A]
-) extends Empty[A]:
-  val empty: A = inst.construct([A] => (F: F[A]) => F.empty)
+trait DerivedEmpty[A] extends Empty[A]:
+  protected def value(): A
+  lazy val empty: A = value()
+
+object DerivedEmpty:
+  type EmptyOrDerived[A] = Empty[A] OrElse DerivedEmpty[A]
+
+  def product[A](using inst: K0.ProductInstances[EmptyOrDerived, A]): DerivedEmpty[A] =
+    () => inst.construct([A] => (F: EmptyOrDerived[A]) => F.unify.empty)
+
+  inline def coproduct[A](using gen: K0.CoproductGeneric[A]): DerivedEmpty[A] =
+    () => K0.summonFirst[EmptyOrDerived, gen.MirroredElemTypes, A].unify.empty
+
+  inline given derived[A](using gen: K0.Generic[A]): DerivedEmpty[A] =
+    inline gen match
+      case given K0.ProductGeneric[A] => product
+      case given K0.CoproductGeneric[A] => coproduct
 
 trait EmptyDerivation:
   extension (E: Empty.type)
-    inline def derived[A](using gen: K0.ProductGeneric[A]): Empty[A] =
-      ProductEmpty(using K0.mkProductInstances)
+    inline def derived[A](using instance: DerivedEmpty[A]): Empty[A] = instance

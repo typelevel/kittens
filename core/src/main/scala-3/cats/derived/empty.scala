5d37c1fe4c95d07abe8de493404dbbd201e28904
Georgi Krastev <joro.kr.21@gmail.com>
2021-05-31 18:10:27 +0300

    Replace OrElse with delegated instance

--- core/src/main/scala-3/cats/derived/empty.scala
+++ core/src/main/scala-3/cats/derived/empty.scala
@@ -6,23 +6,25 @@ import shapeless3.deriving.K0
 object empty extends EmptyDerivation
 
 trait DerivedEmpty[A] extends Empty[A]:
-  protected def value(): A
-  lazy val empty: A = value()
+  protected def emptyValue(): A
+  lazy val empty: A = emptyValue()
 
-object DerivedEmpty:
-  type EmptyOrDerived[A] = Empty[A] OrElse DerivedEmpty[A]
+object DerivedEmpty extends DerivedEmptyLowPriority:
+  given delegated[A](using A: => Empty[A]): DerivedEmpty[A] =
+    () => A.empty
 
-  def product[A](using inst: K0.ProductInstances[EmptyOrDerived, A]): DerivedEmpty[A] =
-    () => inst.construct([A] => (F: EmptyOrDerived[A]) => F.unify.empty)
+  def product[A](using inst: K0.ProductInstances[DerivedEmpty, A]): DerivedEmpty[A] =
+    () => inst.construct([A] => (A: DerivedEmpty[A]) => A.empty)
 
   inline def coproduct[A](using gen: K0.CoproductGeneric[A]): DerivedEmpty[A] =
-    () => K0.summonFirst[EmptyOrDerived, gen.MirroredElemTypes, A].unify.empty
+    K0.summonFirst[DerivedEmpty, gen.MirroredElemTypes, A]
 
+private[derived] sealed abstract class DerivedEmptyLowPriority:
   inline given derived[A](using gen: K0.Generic[A]): DerivedEmpty[A] =
     inline gen match
-      case given K0.ProductGeneric[A] => product
-      case given K0.CoproductGeneric[A] => coproduct
+      case given K0.ProductGeneric[A] => DerivedEmpty.product
+      case given K0.CoproductGeneric[A] => DerivedEmpty.coproduct
 
 trait EmptyDerivation:
   extension (E: Empty.type)
-    inline def derived[A](using instance: DerivedEmpty[A]): Empty[A] = instance
+    def derived[A](using instance: DerivedEmpty[A]): Empty[A] = instance
--- core/src/test/scala-3/cats/derived/EmptyTests.scala
+++ core/src/test/scala-3/cats/derived/EmptyTests.scala
@@ -5,7 +5,7 @@ import cats._
 import cats.derived.all._
 
 class EmptyTests:
-  case class Foo(i: Int, b: Option[IntTree]) derives Empty
+  case class Foo(i: Int, b: IntTree) derives Empty
   enum IntTree:
     case Leaf
     case Node(left: IntTree, value: Int, right: IntTree)

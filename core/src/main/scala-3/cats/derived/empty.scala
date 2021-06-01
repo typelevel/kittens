6b5154dbb53cc521e0142132eaa1347b9453ea5c
Georgi Krastev <joro.kr.21@gmail.com>
2021-06-01 15:01:02 +0300

    Deep and Coproduct Empty derivation (#349)

--- core/src/main/scala-3/cats/derived/empty.scala
+++ core/src/main/scala-3/cats/derived/empty.scala
@@ -5,12 +5,26 @@ import shapeless3.deriving.K0
 
 object empty extends EmptyDerivation
 
-class ProductEmpty[F[x] <: Empty[x], A](
-  using inst: K0.ProductInstances[F, A]
-) extends Empty[A]:
-  val empty: A = inst.construct([A] => (F: F[A]) => F.empty)
+trait DerivedEmpty[A] extends Empty[A]:
+  protected def emptyValue(): A
+  lazy val empty: A = emptyValue()
+
+object DerivedEmpty extends DerivedEmptyLowPriority:
+  given delegated[A](using A: => Empty[A]): DerivedEmpty[A] =
+    () => A.empty
+
+  def product[A](using inst: K0.ProductInstances[DerivedEmpty, A]): DerivedEmpty[A] =
+    () => inst.construct([A] => (A: DerivedEmpty[A]) => A.empty)
+
+  inline def coproduct[A](using gen: K0.CoproductGeneric[A]): DerivedEmpty[A] =
+    K0.summonFirst[DerivedEmpty, gen.MirroredElemTypes, A]
+
+private[derived] sealed abstract class DerivedEmptyLowPriority:
+  inline given derived[A](using gen: K0.Generic[A]): DerivedEmpty[A] =
+    inline gen match
+      case given K0.ProductGeneric[A] => DerivedEmpty.product
+      case given K0.CoproductGeneric[A] => DerivedEmpty.coproduct
 
 trait EmptyDerivation:
   extension (E: Empty.type)
-    inline def derived[A](using gen: K0.ProductGeneric[A]): Empty[A] =
-      ProductEmpty(using K0.mkProductInstances)
+    def derived[A](using instance: DerivedEmpty[A]): Empty[A] = instance
---
+++ core/src/test/scala-3/cats/derived/EmptyTests.scala
@@ -0,0 +1,11 @@
+package cats.derived
+
+import alleycats._
+import cats._
+import cats.derived.all._
+
+class EmptyTests:
+  case class Foo(i: Int, b: IntTree) derives Empty
+  enum IntTree:
+    case Leaf
+    case Node(left: IntTree, value: Int, right: IntTree)

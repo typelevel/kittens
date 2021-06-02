07abf513d911cc1c87236b3ca053910ed6856c28
Ben Plommer <ben.plommer@itv.com>
2021-06-02 13:41:01 +0100

    fix loop

--- core/src/main/scala-3/cats/derived/empty.scala
+++ core/src/main/scala-3/cats/derived/empty.scala
@@ -3,6 +3,7 @@ package cats.derived
 import alleycats.Empty
 import shapeless3.deriving.K0
 import scala.compiletime.summonFrom
+import scala.util.NotGiven
 
 object empty extends EmptyDerivation
 
@@ -10,22 +11,47 @@ trait DerivedEmpty[A] extends Empty[A]:
   protected def emptyValue(): A
   lazy val empty: A = emptyValue()
 
+enum EmptyOrDerived[A]:
+  case L(empty: Empty[A])
+  case R(derived: DerivedEmpty[A])
+
+object EmptyOrDerived:
+  inline given [A]: EmptyOrDerived[A] = summonFrom {
+    case e: Empty[A] => L(e)
+    case d: DerivedEmpty[A] => R(d)
+  }
+
 object DerivedEmpty:
   inline given [A]: DerivedEmpty[A] = summonFrom {
-    case given Empty[A] => delegated
-    case given K0.ProductInstances[DerivedEmpty, A] => product
+    case given K0.ProductInstances[EmptyOrDerived, A] => product
     case given K0.CoproductGeneric[A] => coproduct
   }
-  
-  def delegated[A](using A: => Empty[A]): DerivedEmpty[A] =
-    () => A.empty
 
-  def product[A](using inst: K0.ProductInstances[DerivedEmpty, A]): DerivedEmpty[A] =
-    () => inst.construct([A] => (A: DerivedEmpty[A]) => A.empty)
+  def product[A](using inst: K0.ProductInstances[EmptyOrDerived, A]): DerivedEmpty[A] =
+    () => inst.construct([A] => (A: EmptyOrDerived[A]) =>
+    A match {
+      case EmptyOrDerived.L(e) => e.empty
+      case EmptyOrDerived.R(d) => d.empty
+    })
 
   inline def coproduct[A](using gen: K0.CoproductGeneric[A]): DerivedEmpty[A] =
-    K0.summonFirst[DerivedEmpty, gen.MirroredElemTypes, A]
+    K0.summonFirst[EmptyOrDerived, gen.MirroredElemTypes, A] match {
+      case EmptyOrDerived.L(e) => () => e.empty
+      case EmptyOrDerived.R(d) => d
+    }
 
 trait EmptyDerivation:
   extension (E: Empty.type)
-    def derived[A](using instance: DerivedEmpty[A]): Empty[A] = instance
+    def derived[A](using instance: DerivedEmpty[A]): Empty[A] = 
+      Empty(instance.empty)
+
+
+import empty.derived
+
+case class Foo(i: Int, b: IntTree) derives Empty
+enum IntTree:
+  case Leaf
+  case Node(left: IntTree, value: Int, right: IntTree)
+
+@main def run = 
+  println(Empty[Foo].empty)

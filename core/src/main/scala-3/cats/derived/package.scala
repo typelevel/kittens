bf2768e86dceb7fa814160c95ae2f3f0aebe64aa
Tim Spence <timothywspence@gmail.com>
2021-06-04 16:19:53 +0100

    Inline all the things

--- core/src/main/scala-3/cats/derived/package.scala
+++ core/src/main/scala-3/cats/derived/package.scala
@@ -24,21 +24,21 @@ object semiauto extends
   TraverseDerivation,
   Instances:
 
-  def foldable[F[_]](using ev: DerivedFoldable[F]): Foldable[F] = ev.instance
+  inline def foldable[F[_]](using ev: DerivedFoldable[F]): Foldable[F] = ev.instance
 
-  def functor[F[_]](using ev: DerivedFunctor[F]): Functor[F] = ev.instance
+  inline def functor[F[_]](using ev: DerivedFunctor[F]): Functor[F] = ev.instance
 
-  def reducible[F[_]](using ev: DerivedReducible[F]): Reducible[F] = ev.instance
+  inline def reducible[F[_]](using ev: DerivedReducible[F]): Reducible[F] = ev.instance
 
-  def traverse[F[_]](using ev: DerivedTraverse[F]): Traverse[F] = ev.instance
+  inline def traverse[F[_]](using ev: DerivedTraverse[F]): Traverse[F] = ev.instance
 
 
 object auto:
 
-  given [F[_]](using ev: DerivedFoldable[F]): Foldable[F] = ev.instance
+  inline given [F[_]](using ev: DerivedFoldable[F]): Foldable[F] = ev.instance
 
-  given [F[_]](using ev: DerivedFunctor[F]): Functor[F] = ev.instance
+  inline given [F[_]](using ev: DerivedFunctor[F]): Functor[F] = ev.instance
 
-  given [F[_]](using ev: DerivedReducible[F]): Reducible[F] = ev.instance
+  inline given [F[_]](using ev: DerivedReducible[F]): Reducible[F] = ev.instance
 
-  given [F[_]](using ev: DerivedTraverse[F]): Traverse[F] = ev.instance
+  inline given [F[_]](using ev: DerivedTraverse[F]): Traverse[F] = ev.instance

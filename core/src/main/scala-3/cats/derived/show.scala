afc9dc79086abb7925a9517de86b9971d7db7908
Georgi Krastev <joro.kr.21@gmail.com>
2021-06-07 09:41:45 +0300

    Update sbt and shapeless, use project to derive Show for products

--- core/src/main/scala-3/cats/derived/show.scala
+++ core/src/main/scala-3/cats/derived/show.scala
@@ -6,25 +6,28 @@ import shapeless3.deriving.{Continue, K0, Labelling}
 trait ShowDerivation:
 
   extension (F: Show.type)
-    inline def derived[A](using gen: K0.Generic[A]): Show[A] = gen.derive(productShow, coproductShow)
+    inline def derived[A](using gen: K0.Generic[A]): Show[A] =
+      gen.derive(product, coproduct)
 
-  given productShow[A](using inst : => K0.ProductInstances[Show, A], l: Labelling[A]): Show[A] =
-    (a: A) => {
-      var idx = 0
-      val x = inst.foldLeft[StringBuilder](a)(new StringBuilder(s"${l.label}("))(
-        [t] => (acc: StringBuilder, sh: Show[t], t0: t) => {
-          //elemLabels is backed by an array so this should be fast
-          val res = Continue(acc.append(s"${l.elemLabels(idx)}=${sh.show(t0)}, "))
-          idx = idx + 1
-          res
-          }
-      )
-      if(idx > 0) {x.delete(x.length - 2, x.length)}
-      x.append(")")
-      x.toString()
-    }
+  given product[A](using inst: => K0.ProductInstances[Show, A], labelling: Labelling[A]): Show[A] = a =>
+    val prefix = labelling.label
+    val labels = labelling.elemLabels
+    val n = labels.size
+    if n <= 0 then prefix else
+      val sb = new StringBuilder(prefix)
+      sb.append('(')
+      var i = 0
+      while i < n do
+        sb.append(labels(i))
+        sb.append('=')
+        sb.append(inst.project(a)(i)([t] => (show: Show[t], x: t) => show.show(x)))
+        sb.append(", ")
+        i += 1
 
-  given coproductShow[A](using inst: => K0.CoproductInstances[Show, A]): Show[A] =
-    (a: A) => inst.fold[String](a)(
-      [t] => (sh: Show[t], t0: t) => sh.show(t0)
-    )
+      val l = sb.length
+      sb.delete(l - 2, l)
+      sb.append(')')
+      sb.toString
+
+  given coproduct[A](using inst: => K0.CoproductInstances[Show, A]): Show[A] =
+    inst.fold(_)([t] => (show: Show[t], x: t) => show.show(x))

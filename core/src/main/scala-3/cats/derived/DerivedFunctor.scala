2a001dbb6004cda4c27f058e1191559352682545
Georgi Krastev <joro.kr.21@gmail.com>
2021-10-11 02:38:21 +0200

    Port NonEmptyTraverse derivation to Scala 3

--- core/src/main/scala-3/cats/derived/DerivedFunctor.scala
+++ core/src/main/scala-3/cats/derived/DerivedFunctor.scala
@@ -1,9 +1,9 @@
 package cats.derived
 
-import cats.Functor
+import cats.{Contravariant, Functor}
 import shapeless3.deriving.{Const, K1}
+
 import scala.compiletime.*
-import cats.Contravariant
 
 type DerivedFunctor[F[_]] = Derived[Functor[F]]
 object DerivedFunctor:
@@ -16,18 +16,15 @@ object DerivedFunctor:
     def map[A, B](fa: T)(f: A => B): T = fa
 
   given [F[_], G[_]](using F: Or[F], G: Or[G]): DerivedFunctor[[x] =>> F[G[x]]] =
-    F.unify `compose` G.unify
+    F.unify.compose(G.unify)
 
   given [F[_], G[_]](using F: Contravariant[F], G: Contravariant[G]): DerivedFunctor[[x] =>> F[G[x]]] =
-    F `compose` G
+    F.compose(G)
 
   given [F[_]](using inst: => K1.Instances[Or, F]): DerivedFunctor[F] =
-    new Generic(using inst.unify) {}
+    given K1.Instances[Functor, F] = inst.unify
+    new GenericFunctor[Functor, F] {}
 
-  trait Generic[T[x[_]] <: Functor[x], F[_]](using inst: K1.Instances[T, F])
-    extends Functor[F]:
-
-    def map[A, B](fa: F[A])(f: A => B): F[B] =
-      inst.map(fa: F[A]) { [f[_]] => (tf: T[f], fa: f[A]) =>
-        tf.map(fa)(f)
-      }
+  trait GenericFunctor[T[x[_]] <: Functor[x], F[_]](using inst: K1.Instances[T, F]) extends Functor[F]:
+    override def map[A, B](fa: F[A])(f: A => B): F[B] =
+      inst.map(fa: F[A])([f[_]] => (tf: T[f], fa: f[A]) => tf.map(fa)(f))

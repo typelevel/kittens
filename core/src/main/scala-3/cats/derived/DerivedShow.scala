df2792b69990b16345d27ac70f769ca735700070
​Andrzej Ressel <andrzej.ressel@gmail.com>
2022-01-31 00:36:22 +0100

    Implement Eq derivation for Dotty

---
+++ core/src/main/scala-3/cats/derived/DerivedShow.scala
@@ -0,0 +1,49 @@
+package cats.derived
+
+import cats.Show
+import scala.compiletime.*
+import shapeless3.deriving.{Continue, K0, Labelling}
+import scala.deriving.Mirror
+
+type DerivedShow[A] = Derived[Show[A]]
+object DerivedShow:
+  type Or[A] = Derived.Or[Show[A]]
+
+  inline def apply[A]: Show[A] =
+    import DerivedShow.given
+    summonInline[DerivedShow[A]].instance
+
+  given [A](using inst: K0.ProductInstances[Or, A], labelling: Labelling[A]): DerivedShow[A] =
+    given K0.ProductInstances[Show, A] = inst.unify
+    new Product[Show, A] {}
+
+  given [A](using inst: => K0.CoproductInstances[Or, A]): DerivedShow[A] =
+    given K0.CoproductInstances[Show, A] = inst.unify
+    new Coproduct[Show, A] {}
+
+  trait Product[F[x] <: Show[x], A](using inst: K0.ProductInstances[F, A], labelling: Labelling[A])
+       extends Show[A]:
+    def show(a: A): String =
+      val prefix = labelling.label
+      val labels = labelling.elemLabels
+      val n = labels.size
+      if n <= 0 then prefix else
+        val sb = new StringBuilder(prefix)
+        sb.append('(')
+        var i = 0
+        while i < n do
+          sb.append(labels(i))
+          sb.append(" = ")
+          sb.append(inst.project(a)(i)([t] => (show: F[t], x: t) => show.show(x)))
+          sb.append(", ")
+          i += 1
+
+        val l = sb.length
+        sb.delete(l - 2, l)
+        sb.append(')')
+        sb.toString
+
+  trait Coproduct[F[x] <: Show[x], A](using inst: K0.CoproductInstances[F, A])
+       extends Show[A]:
+    def show(a: A): String =
+      inst.fold(a)([t] => (st: F[t], t: t) => st.show(t))
--- core/src/test/scala-3/cats/derived/ShowTests.scala
+++ core/src/test/scala-3/cats/derived/ShowTests.scala
@@ -2,7 +2,7 @@ package cats.derived
 
 import alleycats.*
 import cats.*
-import cats.derived.semiauto.*
+import cats.derived.*
 
 class ShowTests { //
   case class Foo(i: Int, b: Option[String]) derives Show

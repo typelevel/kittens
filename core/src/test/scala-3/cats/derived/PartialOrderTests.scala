21c41a357d20572b4361763d7c70ee9fb87d86a0
Tim Spence <timothywspence@gmail.com>
2022-05-30 07:42:14 +0100

    Port partial order to new scheme (#480)
    * Port invariant derivation to new scheme
    
    * Port scala 2 invariant tests to scala 3
    
    * Consistent test naming
    
    * WIP extra Invariant instances
    
    * Tests for invariant derivation syntax
    
    * Rename partial order derivation
    
    * Port partial order derivation to new scheme
    
    * Rename to PartialOrderSuite
    
    * Port scala 2 partial order suite to scala 3
    
    Co-authored-by: Georgi Krastev <joro.kr.21@gmail.com>

--- core/src/test/scala-3/cats/derived/PartialOrderTests.scala
+++
@@ -1,9 +0,0 @@
-package cats.derived
-
-import alleycats.*
-import cats.*
-import cats.derived.semiauto.*
-
-class PartialOrderTests { //
-  case class Foo(i: Int, b: Option[String]) derives PartialOrder
-}

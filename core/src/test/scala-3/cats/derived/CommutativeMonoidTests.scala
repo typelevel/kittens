40166c9b1e994aae05b44b83191fcd58ec1eccfe
Georgi Krastev <joro.kr.21@gmail.com>
2022-09-13 04:45:04 +0200

    Extends CommutativeMonoidSuite with derived cases

--- core/src/test/scala-3/cats/derived/CommutativeMonoidTests.scala
+++
@@ -1,8 +0,0 @@
-package cats.derived
-
-import cats.kernel.CommutativeMonoid
-import cats.derived.*
-
-class CommutativeMonoidTests { //
-  case class Foo(i: Int, b: Option[Int]) derives CommutativeMonoid
-}

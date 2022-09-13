8257b51ff3d48ceb944cb7a0418ebfa2e04ec1f3
Georgi Krastev <joro.kr.21@gmail.com>
2022-09-13 10:02:48 +0200

    Extend MonoidKSuite with derived cases

--- core/src/test/scala-3/cats/derived/MonoidKSuite.scala
+++ core/src/test/scala-3/cats/derived/MonoidKSuite.scala
@@ -10,15 +10,15 @@ class MonoidKSuite extends KittensSuite:
   import MonoidKSuite.*
   import TestDefns.*
 
-  inline def monoidKTests[F[_]]: MonoidKTests[F] =
+  inline def tests[F[_]]: MonoidKTests[F] =
     MonoidKTests[F](summonInline)
 
-  inline def testMonoidK(context: String): Unit =
-    checkAll(s"$context.MonoidK[ComplexProduct]", monoidKTests[ComplexProduct].monoidK[Char])
-    checkAll(s"$context.MonoidK[CaseClassWOption]", monoidKTests[CaseClassWOption].monoidK[Char])
-    checkAll(s"$context.MonoidK[BoxMul]", monoidKTests[BoxMul].monoidK[Char])
-    checkAll(s"$context.MonoidK is Serializable", SerializableTests.serializable(summonInline[MonoidK[ComplexProduct]]))
-    test(s"$context.MonoidK respects existing instances") {
+  inline def validate(instance: String): Unit =
+    checkAll(s"$instance[ComplexProduct]", tests[ComplexProduct].monoidK[Char])
+    checkAll(s"$instance[CaseClassWOption]", tests[CaseClassWOption].monoidK[Char])
+    checkAll(s"$instance[BoxMul]", tests[BoxMul].monoidK[Char])
+    checkAll(s"$instance is Serializable", SerializableTests.serializable(summonInline[MonoidK[ComplexProduct]]))
+    test(s"$instance respects existing instances") {
       val M = summonInline[MonoidK[BoxMul]]
       assert(M.empty[Char] == Box(Mul[Char](1)))
       assert(M.combineK(Box(Mul[Char](5)), Box(Mul[Char](5))) == Box(Mul[Char](25)))
@@ -26,12 +26,21 @@ class MonoidKSuite extends KittensSuite:
 
   locally {
     import auto.monoidK.given
-    testMonoidK("auto")
+    validate("auto.monoidK")
   }
 
   locally {
-    import monInstances.given
-    testMonoidK("semi")
+    import semiMonoidK.given
+    validate("semiauto.monoidK")
+  }
+
+  locally {
+    import derivedMonoidK.*
+    val instance = "derived.monoidK"
+    checkAll(s"$instance[ComplexProduct]", tests[ComplexProduct].monoidK[Char])
+    checkAll(s"$instance[CaseClassWOption]", tests[CaseClassWOption].monoidK[Char])
+    checkAll(s"$instance[Simple]", tests[Simple].monoidK[Char])
+    checkAll(s"$instance is Serializable", SerializableTests.serializable(summonInline[MonoidK[ComplexProduct]]))
   }
 
 end MonoidKSuite
@@ -41,22 +50,23 @@ object MonoidKSuite:
 
   type BoxMul[A] = Box[Mul[A]]
 
-  object monInstances:
+  object semiMonoidK:
     given MonoidK[ComplexProduct] = semiauto.monoidK
     given MonoidK[CaseClassWOption] = semiauto.monoidK
     given MonoidK[BoxMul] = semiauto.monoidK
 
+  object derivedMonoidK:
+    case class ComplexProduct[A](x: TestDefns.ComplexProduct[A]) derives MonoidK
+    case class CaseClassWOption[A](x: TestDefns.CaseClassWOption[A]) derives MonoidK
+    case class Simple[A](value1: List[A], value2: Set[A]) derives MonoidK
+    case class Recursive[A](first: List[A], rest: Recursive[A]) derives MonoidK
+
   final case class Mul[T](value: Int)
   object Mul:
     given [T]: Eq[Mul[T]] = Eq.by(_.value)
-
     given [T]: Arbitrary[Mul[T]] = Arbitrary(Arbitrary.arbitrary[Int].map(apply))
-
     given MonoidK[Mul] with
       def empty[A] = Mul(1)
       def combineK[A](x: Mul[A], y: Mul[A]) = Mul(x.value * y.value)
-
-  case class Simple[A](value1: List[A], value2: Set[A]) derives MonoidK
-  case class Recursive[A](first: List[A], rest: Recursive[A]) derives MonoidK
 
 end MonoidKSuite
